c2f5c143c95d0f4b73869c67fa979ced4ac595d9
raytackettsells <ray@doverecovery.com>
2026-06-24 16:02:49 -0400

    Update Import.jsx

--- Import.jsx
+++ Import.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react'
 import * as XLSX from 'xlsx'
-import { supabase } from '../lib/supabase'
+import { supabase } from './supabase'
 
 const MONTHS = [
   'January','February','March','April','May','June',
